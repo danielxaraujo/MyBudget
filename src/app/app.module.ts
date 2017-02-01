8a1a46166e4a3a1db7ddb230e61f1ecc2d6697cd
Daniel Xavier Araújo <danielxaraujo@gmail.com>
2017-02-01 19:42:19 -0300

    Correção

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
-import { CommonModule } from '@angular/common';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing';
@@ -23,8 +21,6 @@ import { BreadcrumbsModule } from "./components/commons/breadcrumbs/breadcrumbs.
 		BrowserModule,
 		FormsModule,
 		HttpModule,
-		RouterModule,
-		CommonModule,
 		AppRoutingModule,
 		BreadcrumbsModule
 	],
--- src/app/components/commons/breadcrumbs/breadcrumbs.module.ts
+++ src/app/components/commons/breadcrumbs/breadcrumbs.module.ts
@@ -1,7 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { BreadcrumbsComponent } from './breadcrumbs.component';
 
 @NgModule({
+	imports: [CommonModule, RouterModule],
 	declarations: [BreadcrumbsComponent],
 	exports: [BreadcrumbsComponent]
 })
