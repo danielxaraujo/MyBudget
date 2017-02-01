d0583b56ccf0d7a06b99b146e2f92997f698ae60
Daniel Xavier Araújo <danielxaraujo@gmail.com>
2017-02-01 19:18:33 -0300

    Correção

--- src/app/components/commons/breadcrumbs/breadcrumbs,.module.ts
+++ src/app/components/commons/breadcrumbs/breadcrumbs,.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { BreadcrumbsComponent } from './breadcrumbs.component';
@@ -6,6 +6,7 @@ import { BreadcrumbsComponent } from './breadcrumbs.component';
 @NgModule({
 	imports: [ CommonModule, RouterModule ],
 	declarations: [BreadcrumbsComponent],
-	exports: [ BreadcrumbsComponent, CommonModule, RouterModule]
+	exports: [ BreadcrumbsComponent],
+	schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class BreadcrumbsModule { }
--- src/app/views/dashboard/dashboard.module.ts
+++ src/app/views/dashboard/dashboard.module.ts
@@ -2,10 +2,10 @@ import { NgModule } from '@angular/core';
 
 import { DashboardComponent } from './dashboard.component';
 import { DashboardRoutingModule } from './dashboard-routing.module';
+import { BreadcrumbsModule } from '../../components/commons/breadcrumbs/breadcrumbs,.module';
 
 @NgModule({
 	declarations: [DashboardComponent],
-	imports: [DashboardRoutingModule],
-
+	imports: [DashboardRoutingModule, BreadcrumbsModule]
 })
 export class DashboardModule { }
