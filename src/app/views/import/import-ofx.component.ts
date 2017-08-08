2e62db661f06361622cfd596209d2f4fb667d4ca
Daniel Xavier Araújo <danielxaraujo@gmail.com>
2017-08-08 19:11:04 -0300

    Evolução

--- src/app/views/import/import-ofx.component.ts
+++ src/app/views/import/import-ofx.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { FileUploader } from 'ng2-file-upload';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { FileUploader, FileItem, ParsedResponseHeaders } from 'ng2-file-upload';
 
 const URL = 'http://127.0.0.1:3000/api/import/ofx';
 
@@ -9,15 +9,26 @@ const URL = 'http://127.0.0.1:3000/api/import/ofx';
 })
 export class ImportOfxComponent implements OnInit {
 
+	isLoading: boolean = false
+	progress: number = 0;
 	uploader: FileUploader = new FileUploader({ url: URL });
 	hasBaseDropZoneOver: boolean = false;
 
-	constructor() { }
+	constructor(private detector: ChangeDetectorRef) { }
 
 	ngOnInit() {
+
+		this.uploader.onCompleteItem = (item: FileItem, response: string, status: number, headers: ParsedResponseHeaders) => {
+			item.progress = 100;
+		};
+
+		this.uploader.onProgressItem = (fileItem: FileItem, progress: any) => {
+			this.progress = progress;
+			this.detector.detectChanges()
+		};
 	}
 
-	public fileOverBase(e: any): void {
+	public fileOver(e: any): void {
 		this.hasBaseDropZoneOver = e;
 	}
 }
